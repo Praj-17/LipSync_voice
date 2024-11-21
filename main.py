df8b1de693332cc6c9166f39186e922d0e760ab9
praj-17 <pwaykos1@gmail.com>
2024-11-21 17:50:39 +0530

    tested both functions at once

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from video_retalking.voice_clone import VoiceCloner
 from video_retalking.inference_module import run_inference
+import warnings
+
+# Suppress all warnings
+warnings.filterwarnings("ignore")
 import sys
+
+voice_cloner = VoiceCloner(speaker_dir="")
 
 
 if __name__ == "__main__":
+    audio = voice_cloner.generate_voice(voice_dir = "speakers/obama", output_path= "output.wav")
     run_inference(
         face='video_retalking/examples/face/7.mp4',
-        audio_path='video_retalking/examples/audio/output_waved_class.wav',
+        audio_path='output.wav',
         outfile='video_retalking/results/obama_7_2.mp4',
     )
